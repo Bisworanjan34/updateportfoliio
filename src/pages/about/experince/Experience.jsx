531f64b81c7963db57e289bb61a38cee219fc2dc
Bisworanjan34 <bisworanjan34@gmail.com>
2024-10-05 16:00:26 +0530

    Update Experience.jsx

--- src/pages/about/experince/Experience.jsx
+++ src/pages/about/experince/Experience.jsx
@@ -3,8 +3,8 @@ import './Experience.css'
 const Experience = () => {
   return (
     <div className='experience-div'>
-      <h5 className='text-center'>FRESHER</h5>
-      <h5>experience</h5>
+      <h5 className='text-center d-title tpurple'>FRESHER</h5>
+      <h5 className=' twht' >experience</h5>
       <p>Now i am a Fresher web Developer but i have work with lot of project like ecommerce web page designing and cart functionality i have familarity with css bootstrap Javascript i have work on whatsapp clon design youtube design i know the concept if you will give me chance then i will prove my self am always trying to do better lear nwe things </p>
     </div>
   )
